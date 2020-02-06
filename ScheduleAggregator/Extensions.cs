344b25fb5e3a85258b3b571d41cfb64a08b0e8ce
Aλexey I.m2strng4dtwrld <inredikawb@gmail.com>
2020-02-06 10:39:06 +0300

    refactor: extract schedule provider

---
+++ ScheduleAggregator/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ItmoScheduleApiWrapper.Models;
+
+namespace ScheduleAggregator
+{
+    public static class Extensions
+    {
+        public static Boolean IsLecture(this ScheduleItemModel model)
+        {
+            return model.Status == "Лек";
+        }
+    }
+}
--- ScheduleAggregator/Program.cs
+++ ScheduleAggregator/Program.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using ItmoScheduleApiWrapper;
-using ItmoScheduleApiWrapper.Models;
-using MoreLinq;
 
 namespace ScheduleAggregator
 {
@@ -13,69 +10,19 @@ namespace ScheduleAggregator
         static void Main()
         {
             Console.OutputEncoding = Encoding.UTF8;
-
-            var scheduleItems = GetGroups()
-                .AsParallel()
-                .Select(GetScheduleItemModels)
-                .SelectMany(e => e)
-                .ToList();
-
+            ScheduleItemProvider provider = new ScheduleItemProvider(GetTmpGroups().ToList());
+            
             Console.WriteLine("\t\tLectures");
-            Print(scheduleItems
-                .Where(e => e.IsLecture())
-                .DistinctBy(e => (e.StartTime, e.SubjectTitle, e.Room))
-                .ToList());
-
+            provider.PrintLecture();
+            
             Console.WriteLine("\n\n\t\tPractice");
-            Print(scheduleItems
-                .Where(e => e.IsLecture() == false)
-                .ToList());
+            provider.PrintPractice();
         }
 
-        public static IEnumerable<ScheduleItemModel> GetScheduleItemModels(String groupTitle)
+        private static IEnumerable<String> GetTmpGroups()
         {
-            ItmoApiProvider provider = new ItmoApiProvider();
+            yield return "M3101";
 
-            var result = provider
-                .ScheduleApi
-                .GetGroupSchedule(groupTitle)
-                .Result
-                .Schedule;
-            result.ForEach(s => s.Group = groupTitle);
-            return result;
-        }
-
-        private static IEnumerable<String> GetGroups()
-        {
-            yield return "M3403";
-            yield return "M3404";
-            yield return "M3405";
-            yield return "M3406";
-            yield return "M3407";
-            yield return "M3408";
-            yield return "M3409";
-        }
-
-        private static Boolean IsLecture(this ScheduleItemModel model)
-        {
-            return model.Status == "Лек";
-        }
-
-        private static void Print(List<ScheduleItemModel> items)
-        {
-            var scheduleItems = items
-                .Select(e => (e.DataDay, e.DataWeek, e.Group, e.StartTime, e.SubjectTitle))
-                .GroupBy(e => (e.DataDay, e.DataWeek))
-                .ToList();
-
-            foreach (var tuple in scheduleItems)
-            {
-                Console.WriteLine($"\t\t{tuple.Key}");
-                foreach (var valueTuple in tuple)
-                {
-                    Console.WriteLine(valueTuple);
-                }
-            }
         }
     }
 }
