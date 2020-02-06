586eb47746ed84db072c8f818c37f06eec1bacce
Aλexey I.m2strng4dtwrld <inredikawb@gmail.com>
2020-02-06 10:46:47 +0300

    feat: add printer abstraction

---
+++ ScheduleAggregator/Printers/IPrinter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace ScheduleAggregator.Printers
+{
+    public interface IPrinter
+    {
+        void Print(string message);
+    }
+
+    public class ConsolePrinter : IPrinter
+    {
+        public void Print(String message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class FilePrinter : IPrinter
+    {
+        private readonly String _filePath;
+
+        public FilePrinter(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void Print(String message)
+        {
+            File.AppendAllText(_filePath, $"{message}\n");
+        }
+    }
+}
