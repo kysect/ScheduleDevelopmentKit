90124f893886696399edbcf8214257a75ee3292f
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2020-02-15 01:34:16 +0300

    feat: add all days to view

--- ScheduleAggregator.Ui/MainWindow.xaml.cs
+++ ScheduleAggregator.Ui/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
+using ItmoScheduleApiWrapper.Models;
 using ItmoScheduleApiWrapper.Types;
 using ScheduleAggregator.Ui.CustomElements;
 
@@ -13,11 +15,25 @@ namespace ScheduleAggregator.Ui
         public MainWindow()
         {
             InitializeComponent();
-            var provider = new ScheduleItemProvider(new List<string>() {"M3101"}, null);
-            List<DaySchedule> schedule = new List<DaySchedule>();
-            schedule.Add(new DaySchedule(provider.GetItemsForGroup(), DataWeekType.Odd, DataDayType.Monday));
-            OddItemList.ItemsSource = schedule;
 
+            var provider = new ScheduleItemProvider(new List<string> {"M3101"}, null);
+            var items = provider.GetItemsForGroup();
+            InitList(OddItemList, DataWeekType.Odd, items);
+            InitList(EvenItemList, DataWeekType.Even, items);
+        }
+
+        public void InitList(ListBox listBox, DataWeekType weekType, List<ScheduleItemModel> items)
+        {
+            List<DaySchedule> schedule = new List<DaySchedule>
+            {
+                new DaySchedule(items, weekType, DataDayType.Monday),
+                new DaySchedule(items, weekType, DataDayType.Tuesday),
+                new DaySchedule(items, weekType, DataDayType.Wednesday),
+                new DaySchedule(items, weekType, DataDayType.Thursday),
+                new DaySchedule(items, weekType, DataDayType.Friday),
+                new DaySchedule(items, weekType, DataDayType.Saturday)
+            };
+            listBox.ItemsSource = schedule;
         }
     }
 }
