61da2c1b6dd57a40ed903f5c69f145b2f072fc14
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2020-09-05 16:58:52 +0300

    feat: add item grouping

---
+++ ScheduleAggregator/ViewItem/SimpleViewItem.cs
@@ -0,0 +1,19 @@
+﻿using ItmoScheduleApiWrapper.Models;
+
+namespace ScheduleAggregator.ViewItem
+{
+    public class SimpleViewItem : IScheduleIViewItem
+    {
+        public ScheduleItemModel Model { get; set; }
+
+        public SimpleViewItem(ScheduleItemModel model)
+        {
+            Model = model;
+        }
+
+        public string ToViewString()
+        {
+            return (Model.StartTime, Model.SubjectTitle, Model.Status, Model.Teacher, Model.Group).ToString();
+        }
+    }
+}
