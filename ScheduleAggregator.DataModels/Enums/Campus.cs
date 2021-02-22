7ac3c2cc53e49794ceec5d1b5826956858c4c282
s4xack <sergmiron47@gmail.com>
2021-02-22 22:28:41 +0300

    feat: review updates

--- ScheduleAggregator.DataModels/Enums/Campus.cs
+++ ScheduleAggregator.DataModels/Enums/Campus.cs
@@ -4,10 +4,10 @@ namespace ScheduleAggregator.DataModels.Enums
 {
     public enum Campus
     {
+        Undefined,
         Lomonosova,
         Kronverskiy,
-        Birjevaya,
-        Undefined
+        Birjevaya
     }
 
     public static class CampusExtensionsMethods
@@ -15,10 +15,10 @@ namespace ScheduleAggregator.DataModels.Enums
         public static string ToRuString(this Campus campus) =>
             campus switch
             {
+                Campus.Undefined => "Неизвестно",
                 Campus.Lomonosova => "улица Ломоносова, д. 9",
                 Campus.Kronverskiy => "Кронверкский проспект, д. 49",
                 Campus.Birjevaya => "Биржевая линия, д. 14",
-                Campus.Undefined => "Неизвестно",
                 _ => throw new ArgumentOutOfRangeException(nameof(campus), campus, null)
             };
     }
