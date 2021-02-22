1f938ef4e21022505f1f8fad929e385eb09c614c
s4xack <sergmiron47@gmail.com>
2021-02-22 21:42:40 +0300

    feat: add base models

---
+++ ScheduleAggregator.DataModels/Semester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace ScheduleAggregator.DataModels
+{
+    public class Semester
+    {
+        public Guid Id { get; set; }
+
+        public string Name;
+        public List<SemesterSubject> Subjects { get; set; }
+
+        public StudyCourse StudyCourse { get; set; }
+    }
+}
