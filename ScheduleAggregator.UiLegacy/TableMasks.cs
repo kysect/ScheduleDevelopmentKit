225d3f6166fa99839b8b6137f2e680f4cbe3832b
Виталий Андреевич Диковицкий <vitaldikov@niuitmo.ru>
2021-02-23 19:11:42 +0300

    add masks

---
+++ ScheduleAggregator.UiLegacy/TableMasks.cs
@@ -0,0 +1,14 @@
+﻿using Kysect.ItmoScheduleSdk.Types;
+using System.Collections.Generic;
+
+namespace ScheduleAggregator.Ui
+{
+    public class TableMasks
+    {
+        public DataWeekType WeekType = DataWeekType.Even;
+        public string StartTime = default;
+        public string Room = default;
+        public string Teacher = default;
+        public string SubjectTitle = default;
+    }
+}
