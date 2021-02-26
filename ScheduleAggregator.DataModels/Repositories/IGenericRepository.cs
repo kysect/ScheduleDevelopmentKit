8be1547d68bc9715618750360402d1477066b087
VitalDikov <vitaldikov@gmail.com>
2021-02-26 15:04:44 +0300

    add repositories

---
+++ ScheduleAggregator.DataModels/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryEF.Data.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        void Create(TEntity item);
+        TEntity FindById(int id);
+        IEnumerable<TEntity> Get();
+        IEnumerable<TEntity> Get(Func<TEntity, bool> predicate);
+        void Remove(TEntity item);
+        void Update(TEntity item);
+    }
+}
---
+++ ScheduleAggregator.DataModels/Repositories/UnitOfWork.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScheduleAggregator.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TryEF.Data.Repositories
+{
+    public class UnitOfWork : IDisposable
+    {
+        private ScheduleContext db;
+        public IGenericRepository<LabourIntensity> LabourIntensities { get;private set; }
+        public IGenericRepository<Lesson> Lessons { get; private set; }
+        public IGenericRepository<Room> Rooms{ get; private set; }
+        public IGenericRepository<Schedule> Schedules{ get; private set; }
+        public IGenericRepository<Semester> Semesters{ get; private set; }
+        public IGenericRepository<SemesterSubject> SemesterSubjects{ get; private set; }
+        public IGenericRepository<StudyCourse> StudyCourses{ get; private set; }
+        public IGenericRepository<Subject> Subjects{ get; private set; }
+        public IGenericRepository<Teacher> Teachers{ get; private set; }
+
+
+        public UnitOfWork()
+        {
+            var options = new DbContextOptionsBuilder<ScheduleContext>()
+            .UseInMemoryDatabase(databaseName: "Test")
+            .Options;
+
+            db = new ScheduleContext(options);
+            LabourIntensities = new GenericRepository<LabourIntensity>(db, db.LabourIntensities);
+            Lessons= new GenericRepository<Lesson>(db, db.Lessons);
+            Rooms = new GenericRepository<Room>(db, db.Rooms);
+            Schedules = new GenericRepository<Schedule>(db, db.Schedules);
+            Semesters = new GenericRepository<Semester>(db, db.Semesters);
+            SemesterSubjects= new GenericRepository<SemesterSubject>(db, db.SemesterSubjects);
+            StudyCourses= new GenericRepository<StudyCourse>(db, db.StudyCourses);
+            Subjects = new GenericRepository<Subject>(db, db.Subjects);
+            Teachers = new GenericRepository<Teacher>(db, db.Teachers);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
