8e4d2c668c8949df362ab6c326d1ce8f7d51ec12
Fredi Kats <fredikats@outlook.com>
2021-03-21 13:29:38 +0300

    feat: implement course adding

--- ScheduleAggregator.Ui/ViewModels/AddCourseViewModel.cs
+++ ScheduleAggregator.Ui/ViewModels/AddCourseViewModel.cs
@@ -1,13 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
 using ScheduleAggregator.DataModels.Entities;
+using ScheduleAggregator.Ui.Commands;
 using ScheduleAggregator.Ui.Tools;
 
 namespace ScheduleAggregator.Ui.ViewModels
 {
-    public class AddCourseViewModel
+    public class AddCourseViewModel : BaseViewModel
     {
-        public List<StudyCourse> Courses => ExecutionContext.Instance.Courses;
+        public AddCourseViewModel()
+        {
+            AddNewCourseCommand = new BaseCommand(_ => AddNewCourse());
+            Courses = new ObservableCollection<StudyCourse>(ExecutionContext.Instance.Courses);
+        }
 
+        public ObservableCollection<StudyCourse> Courses { get; }
 
+        public string NewCourseName { get; set; }
+
+        public ICommand AddNewCourseCommand { get; }
+
+        private void AddNewCourse()
+        {
+            var newCourse = new StudyCourse {Name = NewCourseName};
+            ExecutionContext.Instance.Courses.Add(newCourse);
+            Courses.Add(newCourse);
+            OnPropertyChanged(nameof(Courses));
+        }
     }
 }
