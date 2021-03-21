2da1af1a8615bb62eedf65537d75d16d9dd2d14b
Fredi Kats <inredikawb@gmail.com>
2021-03-21 12:20:52 +0300

    feat: add execution context add loading study courses from it

---
+++ ScheduleAggregator.Ui/ViewModels/AddCourseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using ScheduleAggregator.DataModels.Entities;
+using ScheduleAggregator.Ui.Tools;
+
+namespace ScheduleAggregator.Ui.ViewModels
+{
+    public class AddCourseViewModel
+    {
+        public List<StudyCourse> Courses => ExecutionContext.Instance.Courses;
+
+
+    }
+}
