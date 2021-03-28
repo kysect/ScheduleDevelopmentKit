e3860b36832db7ccf98becc2360d16e0cc9cb194
s4xack <sergmiron47@gmail.com>
2021-03-28 23:22:42 +0300

    chore: move ui to wpf

---
+++ ScheduleAggregator.Desktop/Controls/ScheduleControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ScheduleAggregator.Desktop.Controls
+{
+    public sealed partial class ScheduleControl : UserControl
+    {
+        public ScheduleControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
