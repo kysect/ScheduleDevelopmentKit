12cc079251bbc69a10aadd5b84d3b839f6cf9875
s4xack <sergmiron47@gmail.com>
2021-03-28 23:22:42 +0300

    chore: move ui to wpf

---
+++ ScheduleAggregator.Desktop/Commands/BaseCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ScheduleAggregator.Desktop.Commands
+{
+    public class BaseCommand : ICommand
+    {
+        private readonly Action<object> _action;
+
+        public BaseCommand(Action<object> action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+    public class BaseCommand<T> : ICommand
+    {
+        private readonly Action<T> _action;
+
+        public BaseCommand(Action<T> action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
