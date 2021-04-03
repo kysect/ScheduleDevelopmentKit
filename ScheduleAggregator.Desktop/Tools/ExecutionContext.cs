48db7d1eb02b124ccac2a830ff408125c307fd14
Fredi Kats <fredikats@outlook.com>
2021-04-03 20:11:40 +0300

    chore: merge changes to new UI project

--- ScheduleAggregator.Desktop/Tools/ExecutionContext.cs
+++ ScheduleAggregator.Desktop/Tools/ExecutionContext.cs
@@ -1,17 +1,53 @@
-﻿using System.Collections.Generic;
-using ScheduleAggregator.DataModels.Entities;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ScheduleAggregator.DataModels.Repositories;
+using ScheduleAggregator.DataModels.Services;
 
 namespace ScheduleAggregator.Desktop.Tools
 {
     public class ExecutionContext
     {
-        public static ExecutionContext Instance = new ExecutionContext();
+        public static readonly ExecutionContext Instance = Create();
 
-        public ExecutionContext()
+        private static ExecutionContext Create()
         {
-            Courses = new List<StudyCourse>();
+            var scheduleContext =
+                new ScheduleContext(
+                    new DbContextOptionsBuilder<ScheduleContext>()
+                        .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                        .Options);
+
+            return new ExecutionContext(scheduleContext);
         }
 
-        public List<StudyCourse> Courses { get; set; }
+        private readonly UnitOfWork _unitOfWork;
+
+        public LessonService LessonService { get; private set; }
+        public RoomService RoomService { get; private set; }
+        public ScheduleService ScheduleService { get; private set; }
+        public SemesterService SemesterService { get; private set; }
+        public SemesterSubjectService SemesterSubjectService { get; private set; }
+        public StudyCourseService StudyCourseService { get; private set; }
+        public StudyGroupService StudyGroupService { get; private set; }
+        public SubjectService SubjectService { get; private set; }
+        public TeacherService TeacherService { get; private set; }
+
+        public IsuParser IsuParser { get; private set; }
+
+        public ExecutionContext(ScheduleContext context)
+        {
+            _unitOfWork = new UnitOfWork(context);
+
+            LessonService = new LessonService(_unitOfWork);
+            RoomService = new RoomService(_unitOfWork);
+            ScheduleService = new ScheduleService(_unitOfWork);
+            SemesterService = new SemesterService(_unitOfWork);
+            StudyGroupService = new StudyGroupService(_unitOfWork);
+            StudyCourseService = new StudyCourseService(_unitOfWork);
+            SubjectService = new SubjectService(_unitOfWork);
+            TeacherService = new TeacherService(_unitOfWork);
+            SemesterSubjectService = new SemesterSubjectService(_unitOfWork);
+            IsuParser = new IsuParser(_unitOfWork);
+        }
     }
 }
