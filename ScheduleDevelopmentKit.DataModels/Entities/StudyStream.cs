501992fc8aff9491d7e33b47dc5e215769118ffb
annchous <annchooousss@gmail.com>
2021-09-05 18:13:35 +0300

    feat: add StudyStream entity

---
+++ ScheduleDevelopmentKit.DataModels/Entities/StudyStream.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace ScheduleDevelopmentKit.DataModels.Entities
+{
+    public class StudyStream
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<StudyGroup> Groups { get; set; } = new();
+        public List<Lesson> Lessons { get; set; } = new();
+    }
+}
