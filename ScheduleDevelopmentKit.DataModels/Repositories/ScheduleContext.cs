e4f3350a2280e56e69c7ba8165e9618ab4bbf835
annchous <annchooousss@gmail.com>
2021-09-05 18:14:07 +0300

    feat: register StudyStream entity in database

--- ScheduleDevelopmentKit.DataModels/Repositories/ScheduleContext.cs
+++ ScheduleDevelopmentKit.DataModels/Repositories/ScheduleContext.cs
@@ -10,14 +10,15 @@ namespace ScheduleDevelopmentKit.DataModels.Repositories
         {
         }
 
-        public DbSet<LabourIntensity> LabourIntensities{ get; set; }
-        public DbSet<Lesson> Lessons{ get; set; }
-        public DbSet<Room> Rooms{ get; set; }
-        public DbSet<Schedule> Schedules{ get; set; }
-        public DbSet<Semester> Semesters{ get; set; }
-        public DbSet<SemesterSubject> SemesterSubjects{ get; set; }
-        public DbSet<StudyCourse> StudyCourses{ get; set; }
-        public DbSet<StudyGroup> StudyGroups{ get; set; }
+        public DbSet<LabourIntensity> LabourIntensities { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<Semester> Semesters { get; set; }
+        public DbSet<SemesterSubject> SemesterSubjects { get; set; }
+        public DbSet<StudyCourse> StudyCourses { get; set; }
+        public DbSet<StudyGroup> StudyGroups { get; set; }
+        public DbSet<StudyStream> StudyStreams { get; set; }
         public DbSet<Subject> Subjects { get; set; }
         public DbSet<Teacher> Teachers { get; set; }
     }
